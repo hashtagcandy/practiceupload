ba4db0fc2be6eab2914a19c5b8982ed7705ff2be
go back <go back@VALHALLA>
2021-01-26 14:29:50 +0530

    check if uploaded

---
+++ time/src/time/DateClient.java
@@ -0,0 +1,13 @@
+package time;
+import java.io.*;
+import java.net.*;
+
+public class DateClient {
+	public static void main(String args[]) throws Exception
+    {
+        Socket soc=new Socket(InetAddress.getLocalHost(),5217);        
+        BufferedReader in=new BufferedReader(new InputStreamReader(soc.getInputStream()  ));
+        System.out.println(in.readLine());
+    }    
+
+}
---
+++ time/src/time/DateServer.java
@@ -0,0 +1,22 @@
+package time;
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class DateServer {
+	 public static void main(String args[]) throws Exception
+	    {
+	        ServerSocket s=new ServerSocket(5217);
+	        while(true)
+	        {
+	            System.out.println("Waiting For Connection ...");
+	            Socket soc=s.accept();
+	            DataOutputStream out=new DataOutputStream(soc.getOutputStream());
+	            out.writeBytes("Server Date: " + (new Date()).toString() + "\n");
+	            out.close();
+	            soc.close();
+	        }
+	    }
+	}
+
+
